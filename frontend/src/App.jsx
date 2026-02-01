318e0b94e636b08c2dd5eba4ef969d2fc15d81d2
Ángel Alcántara <angele2705@gmail.com>
2026-02-01 15:43:23 -0600

    Add: Account Page and FIX TYP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import PersonalizationPage from './pages/PersonalizationPage';
 import ImpactPage from './pages/ImpactPage'; 
 import CheckoutPage from './pages/CheckoutPage'; 
 import ThankYouPage from './pages/ThankYouPage';
+import AccountPage from './pages/AccountPage';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         <Route path="/checkout" element={<CheckoutPage />} />
 
         <Route path="/thank-you" element={<ThankYouPage />} />
+
+        <Route path="/account" element={<AccountPage />} />
       </Routes>
     </BrowserRouter>
   )
